4c823bab9739b2f97aab5ae849bde9f877169a9a
Mamadou DIOP <bossiel@yahoo.fr>
2015-12-29 03:35:02 +0100

    -

--- tests/parallel/parallel.cxx
+++ tests/parallel/parallel.cxx
@@ -3,28 +3,55 @@
 
 using namespace compv;
 
+static struct thread {
+	const char* name;
+	CompVObjWrapper<CompVThread*> thread_;
+	CompVObjWrapper<CompVSemaphore*> sema_;
+} threads[] = {
+	{ "foo", NULL, NULL },
+	{ "bar", NULL, NULL },
+	{ "comv", NULL, NULL },
+	{ "doubango", NULL, NULL },
+	{ "telecom", NULL, NULL }
+};
+
 static void* COMPV_STDCALL thread_start(void *arg)
 {
-	COMPV_DEBUG_INFO("Entering thread '%s'", (const char*)arg);
-	CompVThread::sleep(1000);
-	COMPV_DEBUG_INFO("Exiting thread '%s'", (const char*)arg);
+	const struct thread* thread_ = (const struct thread*)arg;
+	COMPV_DEBUG_INFO("Entering thread '%s'", thread_->name);
+	COMPV_CHECK_CODE_ASSERT(thread_->sema_->decrement()); // wait until increment() is called
+	COMPV_DEBUG_INFO("Exiting thread '%s'", thread_->name);
 	return NULL;
 }
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	static const char* thread_names[] = {
-		"foo", "bar", "compv", "doubango", "telecom"
-	};
-
-	{
-		CompVObjWrapper<CompVThread*> thread[sizeof(thread_names) / sizeof(thread_names[0])];
-		for (size_t i = 0; i < sizeof(thread_names) / sizeof(thread_names[0]); ++i) {
-			CompVThread::newObj(&thread[i], thread_start, (void*)thread_names[i]);
-		}
-		getchar();
+	// Create all semaphores
+	for (size_t i = 0; i < sizeof(threads) / sizeof(threads[0]); ++i) {
+		COMPV_CHECK_CODE_ASSERT(CompVSemaphore::newObj(&threads[i].sema_));
 	}
 
+	// Create threads, they will hang as sema.dec() is called until sema.inc() is called
+	for (size_t i = 0; i < sizeof(threads) / sizeof(threads[0]); ++i) {
+		COMPV_CHECK_CODE_ASSERT(CompVThread::newObj(&threads[i].thread_, thread_start, (void*)&threads[i]));
+	}
+
+	// Press a key to continue
+	getchar();
+	
+	// Increment semaphores
+	for (size_t i = 0; i < sizeof(threads) / sizeof(threads[0]); ++i) {
+		COMPV_CHECK_CODE_ASSERT(threads[i].sema_->increment());
+	}
+
+	getchar();
+
+	// destroy all threads and semaphores
+	for (size_t i = 0; i < sizeof(threads) / sizeof(threads[0]); ++i) {
+		threads[i] = { NULL, NULL, NULL };
+	}
+
+	// Press a key to continue
 	getchar();
 
 	return 0;
