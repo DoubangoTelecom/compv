1bc8fd9d51575386034597dbc0ce758883fe22ef
Mamadou DIOP <bossiel@yahoo.fr>
2016-06-13 13:22:56 +0200

    -

--- assembler/x86/math/compv_math_matrix_mul_x86_sse.s
+++ assembler/x86/math/compv_math_matrix_mul_x86_sse.s
@@ -160,13 +160,13 @@ sym(MatrixMaxAbsOffDiagSymm_float64_Asm_X86_SSE41):
 	;; end prolog ;;
 
 	; xmm4 = xmmAbsMask
-	movdqa xmm4, [sym(kAVXFloat64MaskAbs)]
+	movapd xmm4, [sym(kAVXFloat64MaskAbs)]
 
 	; xmm3 = xmmAllZerosMask
-	pcmpeqd xmm3, xmm3 ; 0xfff....
+	cmppd xmm3, xmm3, 0x0 ; 0xfff....
 
 	; xmm5 = xmmMax
-	pxor xmm5, xmm5
+	xorpd xmm5, xmm5
 	
 	mov rcx, arg(6) ; strideInBytes
 	mov rax, arg(4) ; rowStart
@@ -194,27 +194,25 @@ sym(MatrixMaxAbsOffDiagSymm_float64_Asm_X86_SSE41):
 		;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;	
 		.LoopCols1
 			movapd xmm0, [rdx + rdi * 8]
-			pand xmm0, xmm4
-			movapd xmm1, xmm5 ; move all bytes "as is", same as movapd
+			andpd xmm0, xmm4
+			movapd xmm1, xmm5
 			cmppd xmm1, xmm0, 1 ; testing lT instead of GT
 			ptest xmm1, xmm3
+			lea rdi, [rdi + 2] ; increment i
 			jz .LoopCols1NotGreater1
 				comisd xmm0, xmm5
+				mov rcx, rsi ; update row
 				pshufd xmm1, xmm0, 0x4E ; swap first and second doube -> high first then low
 				jbe .LoopCols1NotGreater2
 					pshufd xmm5, xmm0, 0x44 ; duplicate low 8bytes
-					mov rbx, rdi
-					mov rcx, rsi
+					lea rbx, [rdi - 2] ; update col = i + 0
 				.LoopCols1NotGreater2
 				comisd xmm1, xmm5
 				jbe .LoopCols1NotGreater3
+					lea rbx, [rdi - 1] ; update col = i + 1
 					pshufd xmm5, xmm1, 0x44 ; duplicate low 8bytes
-					mov rbx, rdi
-					mov rcx, rsi
-					inc rbx
 				.LoopCols1NotGreater3
 			.LoopCols1NotGreater1
-			add rdi, 2
 			cmp rdi, rax ; i <? j -1
 			jl .LoopCols1
 		.EndOfLoopCols1
@@ -229,12 +227,12 @@ sym(MatrixMaxAbsOffDiagSymm_float64_Asm_X86_SSE41):
 			movsd xmm0, [rdx + rdi * 8] ; 8 = sizeof(#1 double)
 			pand xmm0, xmm4
 			comisd xmm0, xmm5
+			lea rdi, [rdi + 1] ; increment i
 			jbe .LoopCols2NotGreater1
 				pshufd xmm5, xmm0, 0x44 ; duplicate low 8bytes
-				mov rbx, rdi
-				mov rcx, rsi
+				lea rbx, [rdi - 1] ; update col = i + 0
+				mov rcx, rsi ; update row
 			.LoopCols2NotGreater1
-			inc rdi
 			cmp rdi, rsi ; i <? j
 			jl .LoopCols2
 		.EndOfLoopCols2
@@ -273,118 +271,5 @@ sym(MatrixMaxAbsOffDiagSymm_float64_Asm_X86_SSE41):
 ; arg(6) -> compv_uscalar_t strideInBytes
 ; void MatrixMaxAbsOffDiagSymm_float64_Asm_X86_SSE2(const COMPV_ALIGNED(SSE) compv_float64_t* S, compv_uscalar_t *row, compv_uscalar_t *col, compv_float64_t* max, compv_uscalar_t rowStart, compv_uscalar_t rowEnd, compv_uscalar_t strideInBytes)
 sym(MatrixMaxAbsOffDiagSymm_float64_Asm_X86_SSE2):
-	push rbp
-	mov rbp, rsp
-	COMPV_YASM_SHADOW_ARGS_TO_STACK 7
-	push rsi
-	push rdi
-	push rbx
-	;; end prolog ;;
-
-	; alloc memory
-	sub rsp, 8
-	; [rsp + 0] = j - 1
-
-	; xmm4 = xmmAbsMask
-	movdqa xmm4, [sym(kAVXFloat64MaskAbs)]
-
-	; xmm3 = xmmAllZerosMask
-	pcmpeqd xmm3, xmm3 ; 0xfff....
-
-	; xmm5 = xmmMax
-	pxor xmm5, xmm5
 	
-	mov rcx, arg(6) ; strideInBytes
-	mov rax, arg(4) ; rowStart
-	imul rax, rcx 
-	mov rdx, arg(0)
-	add rdx, rax ; rdx = S0_
-
-	xor rcx, rcx ; rcx = row
-	xor rbx, rbx ; rbx = col
-
-	mov rsi, arg(4) ; rsi = j = rowStart
-
-	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-	; for (j = rowStart; j < rowEnd; ++j)
-	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-	.LoopRows
-		xor rdi, rdi ; rdi = i = 0
-
-		lea rax, [rsi - 1]
-		cmp rdi, rax
-		jge .EndOfLoopCols1
-		mov [rsp + 0], rax
-		
-		;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-		; for (; i < j - 1; i += 2)
-		;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;	
-		.LoopCols1
-			movapd xmm0, [rdx + rdi * 8]
-			pand xmm0, xmm4
-			ptest xmm0, xmm3
-			jz .LoopCols1NotGreater1
-				comisd xmm0, xmm5
-				pshufd xmm1, xmm0, 0x4E ; swap first and second doube -> high first then low
-				jbe .LoopCols1NotGreater2
-					pshufd xmm5, xmm0, 0x44 ; duplicate low 8bytes
-					mov rbx, rdi
-					mov rcx, rsi
-				.LoopCols1NotGreater2
-				comisd xmm1, xmm5
-				jbe .LoopCols1NotGreater3
-					pshufd xmm5, xmm1, 0x44 ; duplicate low 8bytes
-					mov rbx, rdi
-					mov rcx, rsi
-					inc rbx
-				.LoopCols1NotGreater3
-			.LoopCols1NotGreater1
-			lea rdi, [rdi + 2]
-			cmp rdi, [rsp + 0] ; i <? j -1
-			jl .LoopCols1
-		.EndOfLoopCols1
-		
-		cmp rdi, rsi ; i <? j
-		jge .EndOfLoopCols2
-
-		;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-		; for (; i < j; i += 1)
-		;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-		.LoopCols2
-			movsd xmm0, [rdx + rdi * 8] ; 8 = sizeof(#1 double)
-			pand xmm0, xmm4
-			comisd xmm0, xmm5
-			jbe .LoopCols2NotGreater1
-				pshufd xmm5, xmm0, 0x44 ; duplicate low 8bytes
-				mov rbx, rdi
-				mov rcx, rsi
-			.LoopCols2NotGreater1
-			inc rdi
-			cmp rdi, rsi ; i <? j
-			jl .LoopCols2
-		.EndOfLoopCols2
-		
-		inc rsi
-		add rdx, arg(6) ; S0_ += strideInBytes
-		cmp rsi, arg(5) ; rsi <? rowEng
-		jl .LoopRows
-	.EndOfLoopRows
-
-	mov rax, arg(3) ; max
-	mov rsi, arg(1) ; row
-	mov rdi, arg(2) ; col
-	movsd [rax], xmm5
-	mov [rsi], rcx
-	mov [rdi], rbx
-
-	; free memory
-	add rsp, 8
-
-	;; begin epilog ;;
-	pop rbx
-	pop rdi
-	pop rsi
-	COMPV_YASM_UNSHADOW_ARGS
-	mov rsp, rbp
-	pop rbp
 	ret
