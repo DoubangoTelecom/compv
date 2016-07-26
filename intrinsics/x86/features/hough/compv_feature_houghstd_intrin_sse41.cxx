4ffc262e9c98d3b9686afe4376f711098af1455d
Doubango Telecom <github@doubango.org>
2016-07-26 22:57:49 +0200

    -

--- intrinsics/x86/features/hough/compv_feature_houghstd_intrin_sse41.cxx
+++ intrinsics/x86/features/hough/compv_feature_houghstd_intrin_sse41.cxx
@@ -21,11 +21,13 @@ void HoughStdAccGatherRow_Intrin_SSE41(int32_t* pACC, int32_t accStride, COMPV_A
 	int32_t col, theta, rhoInt32;
 	const int32_t maxThetaCountSSE = maxThetaCount - 3;
 	const int32_t maxColsSSE = maxCols - 15;
-	__m128i xmmCol, xmmRho, xmmTheta, xmm0;
+	__m128i xmmCol, xmmRho, xmmTheta;
 	const __m128i xmmRow = _mm_set1_epi32(row);
 	const __m128i xmmStride = _mm_set1_epi32(accStride);
 	static const __m128i xmm4 = _mm_set1_epi32(4);
 	int m0, mi;
+	COMPV_ALIGN_SSE() int32_t idx[4];
+	__m128i *xmmIdx = (__m128i*)idx;
 	for (col = 0; col < maxColsSSE; col += 16) {
 		m0 = _mm_movemask_epi8(_mm_load_si128(reinterpret_cast<const __m128i*>(&pixels[col])));
 		if (m0) {
@@ -36,12 +38,12 @@ void HoughStdAccGatherRow_Intrin_SSE41(int32_t* pACC, int32_t accStride, COMPV_A
 					for (theta = 0; theta < maxThetaCountSSE; theta += 4) {
 						xmmRho = _mm_srai_epi32(_mm_add_epi32(_mm_mullo_epi32(xmmCol, _mm_load_si128(reinterpret_cast<const __m128i*>(&pCosRho[theta]))),
 							_mm_mullo_epi32(xmmRow, _mm_load_si128(reinterpret_cast<const __m128i*>(&pSinRho[theta])))), 16);
-						xmm0 = _mm_sub_epi32(xmmTheta, _mm_mullo_epi32(xmmRho, xmmStride));
-						pACC[_mm_extract_epi32(xmm0, 0)]++;
-						pACC[_mm_extract_epi32(xmm0, 1)]++;
-						pACC[_mm_extract_epi32(xmm0, 2)]++;
-						pACC[_mm_extract_epi32(xmm0, 3)]++;
+						_mm_store_si128(xmmIdx, _mm_sub_epi32(xmmTheta, _mm_mullo_epi32(xmmRho, xmmStride)));
+						pACC[idx[0]]++;
+						pACC[idx[1]]++;
 						xmmTheta = _mm_add_epi32(xmmTheta, xmm4);
+						pACC[idx[2]]++;
+						pACC[idx[3]]++;
 					}
 					for (; theta < maxThetaCount; ++theta) {
 						rhoInt32 = (col * pCosRho[theta] + row * pSinRho[theta]) >> 16;
