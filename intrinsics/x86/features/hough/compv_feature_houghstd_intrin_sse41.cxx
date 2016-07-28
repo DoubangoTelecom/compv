2c08b633dec632bad0f798a645caeac7d0379381
Doubango Telecom <github@doubango.org>
2016-07-28 05:14:10 +0200

    -

--- intrinsics/x86/features/hough/compv_feature_houghstd_intrin_sse41.cxx
+++ intrinsics/x86/features/hough/compv_feature_houghstd_intrin_sse41.cxx
@@ -22,6 +22,9 @@ void HoughStdAccGatherRow_Intrin_SSE41(int32_t* pACC, int32_t accStride, COMPV_A
 	const int32_t maxThetaCountSSE = maxThetaCount - 3;
 	const int32_t maxColsSSE = maxCols - 15;
 	__m128i xmmCol, xmmRho, xmmTheta, xmm0;
+#if COMPV_ARCH_X64
+	__m128i xmm1;
+#endif
 	const __m128i xmmRow = _mm_set1_epi32(row);
 	const __m128i xmmStride = _mm_set1_epi32(accStride);
 	static const __m128i xmm4 = _mm_set1_epi32(4);
@@ -36,11 +39,22 @@ void HoughStdAccGatherRow_Intrin_SSE41(int32_t* pACC, int32_t accStride, COMPV_A
 					for (theta = 0, xmmTheta = xmm0123; theta < maxThetaCountSSE; theta += 4, xmmTheta = _mm_add_epi32(xmmTheta, xmm4)) {
 						xmmRho = _mm_srai_epi32(_mm_add_epi32(_mm_mullo_epi32(xmmCol, _mm_load_si128(reinterpret_cast<const __m128i*>(&pCosRho[theta]))),
 							_mm_mullo_epi32(xmmRow, _mm_load_si128(reinterpret_cast<const __m128i*>(&pSinRho[theta])))), 16);
+#if COMPV_ARCH_X64
+						// This code avoid using "movsxd" to transfer memory indexes
+						xmm1 = _mm_sub_epi32(xmmTheta, _mm_mullo_epi32(xmmRho, xmmStride));
+						xmm0 = _mm_cvtepi32_epi64(xmm1);
+						xmm1 = _mm_cvtepi32_epi64(_mm_srli_si128(xmm1, 8));
+						pACC[_mm_cvtsi128_si64(xmm0)]++;
+						pACC[_mm_cvtsi128_si64(_mm_srli_si128(xmm0, 8))]++;
+						pACC[_mm_cvtsi128_si64(xmm1)]++;
+						pACC[_mm_cvtsi128_si64(_mm_srli_si128(xmm1, 8))]++;
+#else
 						xmm0 = _mm_sub_epi32(xmmTheta, _mm_mullo_epi32(xmmRho, xmmStride));
 						pACC[_mm_cvtsi128_si32(xmm0)]++;
 						pACC[_mm_extract_epi32(xmm0, 1)]++;
 						pACC[_mm_extract_epi32(xmm0, 2)]++;
 						pACC[_mm_extract_epi32(xmm0, 3)]++;
+#endif
 					}
 					for (; theta < maxThetaCount; ++theta) {
 						rhoInt32 = (col * pCosRho[theta] + row * pSinRho[theta]) >> 16;
