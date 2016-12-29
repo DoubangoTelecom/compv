af86a132d906057f22a34f4b2ddb32ee25125ddf
Doubango Telecom <github@doubango.org>
2016-12-29 21:19:38 +0100

    -

--- base/image/compv_image_conv_rgbfamily.cxx
+++ base/image/compv_image_conv_rgbfamily.cxx
@@ -40,9 +40,11 @@ COMPV_NAMESPACE_BEGIN()
 #	endif /* COMPV_ARCH_X64 */
 #if COMPV_ARCH_ARM
 		COMPV_EXTERNC void CompVImageConvRgb24family_to_y_Asm_NEON32(COMPV_ALIGNED(NEON) const uint8_t* rgb24Ptr, COMPV_ALIGNED(NEON) uint8_t* outYPtr, compv_uscalar_t width, compv_uscalar_t height, COMPV_ALIGNED(NEON) compv_uscalar_t stride, COMPV_ALIGNED(DEFAULT) const int8_t* kRGBfamilyToYUV_YCoeffs8);
-		COMPV_EXTERNC void CompVImageConvRgb32family_to_y_Asm_NEON32(COMPV_ALIGNED(NEON) const uint8_t* rgb24Ptr, COMPV_ALIGNED(NEON) uint8_t* outYPtr, compv_uscalar_t width, compv_uscalar_t height, COMPV_ALIGNED(NEON) compv_uscalar_t stride, COMPV_ALIGNED(DEFAULT) const int8_t* kRGBfamilyToYUV_YCoeffs8);
+		COMPV_EXTERNC void CompVImageConvRgba32family_to_y_Asm_NEON32(COMPV_ALIGNED(NEON) const uint8_t* rgb24Ptr, COMPV_ALIGNED(NEON) uint8_t* outYPtr, compv_uscalar_t width, compv_uscalar_t height, COMPV_ALIGNED(NEON) compv_uscalar_t stride, COMPV_ALIGNED(DEFAULT) const int8_t* kRGBfamilyToYUV_YCoeffs8);
+		COMPV_EXTERNC void CompVImageConvArgb32family_to_y_Asm_NEON32(COMPV_ALIGNED(NEON) const uint8_t* rgb24Ptr, COMPV_ALIGNED(NEON) uint8_t* outYPtr, compv_uscalar_t width, compv_uscalar_t height, COMPV_ALIGNED(NEON) compv_uscalar_t stride, COMPV_ALIGNED(DEFAULT) const int8_t* kRGBfamilyToYUV_YCoeffs8);
 		COMPV_EXTERNC void CompVImageConvRgb24family_to_uv_planar_11_Asm_NEON32(COMPV_ALIGNED(NEON) const uint8_t* rgb24Ptr, COMPV_ALIGNED(NEON) uint8_t* outUPtr, COMPV_ALIGNED(NEON) uint8_t* outVPtr, compv_uscalar_t width, compv_uscalar_t height, COMPV_ALIGNED(NEON) compv_uscalar_t stride, COMPV_ALIGNED(DEFAULT) const int8_t* kRGBfamilyToYUV_UCoeffs8, COMPV_ALIGNED(DEFAULT) const int8_t* kRGBfamilyToYUV_VCoeffs8);
-		COMPV_EXTERNC void CompVImageConvRgb32family_to_uv_planar_11_Asm_NEON32(COMPV_ALIGNED(NEON) const uint8_t* rgb24Ptr, COMPV_ALIGNED(NEON) uint8_t* outUPtr, COMPV_ALIGNED(NEON) uint8_t* outVPtr, compv_uscalar_t width, compv_uscalar_t height, COMPV_ALIGNED(NEON) compv_uscalar_t stride, COMPV_ALIGNED(DEFAULT) const int8_t* kRGBfamilyToYUV_UCoeffs8, COMPV_ALIGNED(DEFAULT) const int8_t* kRGBfamilyToYUV_VCoeffs8);
+		COMPV_EXTERNC void CompVImageConvRgba32family_to_uv_planar_11_Asm_NEON32(COMPV_ALIGNED(NEON) const uint8_t* rgb24Ptr, COMPV_ALIGNED(NEON) uint8_t* outUPtr, COMPV_ALIGNED(NEON) uint8_t* outVPtr, compv_uscalar_t width, compv_uscalar_t height, COMPV_ALIGNED(NEON) compv_uscalar_t stride, COMPV_ALIGNED(DEFAULT) const int8_t* kRGBfamilyToYUV_UCoeffs8, COMPV_ALIGNED(DEFAULT) const int8_t* kRGBfamilyToYUV_VCoeffs8);
+		COMPV_EXTERNC void CompVImageConvArgb32family_to_uv_planar_11_Asm_NEON32(COMPV_ALIGNED(NEON) const uint8_t* rgb24Ptr, COMPV_ALIGNED(NEON) uint8_t* outUPtr, COMPV_ALIGNED(NEON) uint8_t* outVPtr, compv_uscalar_t width, compv_uscalar_t height, COMPV_ALIGNED(NEON) compv_uscalar_t stride, COMPV_ALIGNED(DEFAULT) const int8_t* kRGBfamilyToYUV_UCoeffs8, COMPV_ALIGNED(DEFAULT) const int8_t* kRGBfamilyToYUV_VCoeffs8);
 #endif
 #endif /* COMPV_ASM */
 
@@ -263,7 +265,11 @@ void __rgb32family_to_y(const uint8_t* rgbaPtr, uint8_t* outYPtr, compv_uscalar_
 #elif COMPV_ARCH_ARM
 	if (CompVCpu::isEnabled(kCpuFlagARM_NEON) && COMPV_IS_ALIGNED_NEON(rgbaPtr) && COMPV_IS_ALIGNED_NEON(outYPtr) && COMPV_IS_ALIGNED_NEON(stride)) {
 		COMPV_EXEC_IFDEF_INTRIN_ARM(funcptr = CompVImageConvRgb32family_to_y_Intrin_NEON);
-		COMPV_EXEC_IFDEF_ASM_ARM(funcptr = CompVImageConvRgb32family_to_y_Asm_NEON32);
+		COMPV_EXEC_IFDEF_ASM_ARM((
+			funcptr = (kRGBAfamilyToYUV_YCoeffs8 == kRGBAToYUV_YCoeffs8 || kRGBAfamilyToYUV_YCoeffs8 == kBGRAToYUV_YCoeffs8)
+			? CompVImageConvRgba32family_to_y_Asm_NEON32 // alpha at the last position and R,G,B at in order
+			: CompVImageConvArgb32family_to_y_Asm_NEON32 // alpha at the first position and R,G,B at in order
+		));
 	}
 #endif
 	funcptr(rgbaPtr, outYPtr, width, height, stride, kRGBAfamilyToYUV_YCoeffs8);
@@ -300,7 +306,11 @@ static void __rgb32family_to_uv_planar_11(const uint8_t* rgbaPtr, uint8_t* outUP
 #elif COMPV_ARCH_ARM
 	if (CompVCpu::isEnabled(kCpuFlagARM_NEON) && COMPV_IS_ALIGNED_NEON(rgbaPtr) && COMPV_IS_ALIGNED_NEON(outUPtr) && COMPV_IS_ALIGNED_NEON(outVPtr) && COMPV_IS_ALIGNED_NEON(stride)) {
 		COMPV_EXEC_IFDEF_INTRIN_ARM(funcptr = CompVImageConvRgb32family_to_uv_planar_11_Intrin_NEON);
-		COMPV_EXEC_IFDEF_ASM_ARM(funcptr = CompVImageConvRgb32family_to_uv_planar_11_Asm_NEON32);
+		COMPV_EXEC_IFDEF_ASM_ARM((
+			funcptr = (kRGBfamilyToYUV_UCoeffs8 == kRGBAToYUV_UCoeffs8 || kRGBfamilyToYUV_UCoeffs8 == kBGRAToYUV_UCoeffs8)
+			? CompVImageConvRgba32family_to_uv_planar_11_Asm_NEON32 // alpha at the last position and R,G,B at in order
+			: CompVImageConvArgb32family_to_uv_planar_11_Asm_NEON32 // alpha at the first position and R,G,B at in order
+			));
 	}
 #endif
 	funcptr(rgbaPtr, outUPtr, outVPtr, width, height, stride, kRGBfamilyToYUV_UCoeffs8, kRGBfamilyToYUV_VCoeffs8);
