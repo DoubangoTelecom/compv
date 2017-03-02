fd9f1b699164b466fc3a3f557b3a47db8983ba6f
Doubango Telecom <github@doubango.org>
2017-03-02 19:37:53 +0100

    -

--- unittests/math_matrix_ops.cxx
+++ unittests/math_matrix_ops.cxx
@@ -105,11 +105,19 @@ static COMPV_ERROR_CODE __math_matrix_ops_mulGA()
 		const char* md5_fma;
 	}
 	COMPV_UNITTEST_MULGA_FLOAT64[] = {
+#if COMPV_ARCH_X86
 		{ 215, 215, "1d28996c99db6fdb058a487ed8a57c45", "32436ce316ff4b10a0becf87da478755" },
 		{ 19, 21, "1d28996c99db6fdb058a487ed8a57c45", "32436ce316ff4b10a0becf87da478755" },
 		{ 701, 71, "1d28996c99db6fdb058a487ed8a57c45", "32436ce316ff4b10a0becf87da478755" },
 		{ 31, 31, "1d28996c99db6fdb058a487ed8a57c45", "32436ce316ff4b10a0becf87da478755" }, // 31 = (16 + 8 + 4 + 2 + 1) -> test all cases
 		{ 9, 9, "1d28996c99db6fdb058a487ed8a57c45", "32436ce316ff4b10a0becf87da478755" }, // Homography
+#elif COMPV_ARCH_ARM
+		{ 215, 215, "1d28996c99db6fdb058a487ed8a57c45", "1ec77d8622cfe7837e56baeb0ef08a0c" },
+		{ 19, 21, "1d28996c99db6fdb058a487ed8a57c45", "1ec77d8622cfe7837e56baeb0ef08a0c" },
+		{ 701, 71, "1d28996c99db6fdb058a487ed8a57c45", "1ec77d8622cfe7837e56baeb0ef08a0c" },
+		{ 31, 31, "1d28996c99db6fdb058a487ed8a57c45", "1ec77d8622cfe7837e56baeb0ef08a0c" }, // 31 = (16 + 8 + 4 + 2 + 1) -> test all cases
+		{ 9, 9, "1d28996c99db6fdb058a487ed8a57c45", "1ec77d8622cfe7837e56baeb0ef08a0c" }, // Homography
+#endif
 	},
 	COMPV_UNITTEST_MULGA_FLOAT32[] = {
 #if COMPV_ARCH_X86
@@ -147,11 +155,7 @@ static COMPV_ERROR_CODE __math_matrix_ops_mulGA()
 				COMPV_CHECK_CODE_RETURN(CompVMatrix::mulGA<T>(A, ith, jth, static_cast<T>(-0.9855), static_cast<T>(0.777774)));
 			}
 		}
-#	if COMPV_ARCH_X86
 		const bool fma = compv_tests_is_fma_enabled() && CompVCpu::isAsmEnabled(); // no FMA3 intrin impl.
-#	else
-		const bool fma = false; // FMA not enabled for ARM yet
-#	endif
 		COMPV_CHECK_EXP_RETURN(std::string(fma ? test->md5_fma : test->md5).compare(compv_tests_md5(A)) != 0, COMPV_ERROR_CODE_E_UNITTEST_FAILED, "Matrix ops mulGA: MD5 mismatch");
 		COMPV_DEBUG_INFO_EX(TAG_TEST, "** Test OK **");
 	}
