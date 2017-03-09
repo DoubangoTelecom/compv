3d82c84e79f24f2946294f824fee1bf7ca94131c
github@doubango.org <github@doubango.org>
2017-03-09 07:37:50 +0100

    -

--- base/math/compv_math_utils.cxx
+++ base/math/compv_math_utils.cxx
@@ -225,6 +225,7 @@ COMPV_ERROR_CODE CompVMathUtils::sumAbs(const int16_t* a, const int16_t* b, uint
 	if (COMPV_IS_ALIGNED_NEON(strideInBytes) && COMPV_IS_ALIGNED_NEON(a) && COMPV_IS_ALIGNED_NEON(b) && COMPV_IS_ALIGNED_NEON(r)) {
 		if (CompVCpu::isEnabled(kCpuFlagARM_NEON)) {
 			COMPV_EXEC_IFDEF_INTRIN_ARM(CompVMathUtilsSumAbs_16s16u = CompVMathUtilsSumAbs_16s16u_Intrin_NEON);
+            COMPV_EXEC_IFDEF_ASM_ARM32(CompVMathUtilsSumAbs_16s16u = CompVMathUtilsSumAbs_16s16u_Asm_NEON32);
 		}
 	}
 #endif
