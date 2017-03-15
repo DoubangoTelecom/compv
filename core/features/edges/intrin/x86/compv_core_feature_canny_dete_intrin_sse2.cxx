5f600bc293b356104de44d090d5b87addca428bb
Doubango Telecom <github@doubango.org>
2017-03-15 18:16:59 +0100

    -

--- core/features/edges/intrin/x86/compv_core_feature_canny_dete_intrin_sse2.cxx
+++ core/features/edges/intrin/x86/compv_core_feature_canny_dete_intrin_sse2.cxx
@@ -11,12 +11,90 @@
 
 COMPV_NAMESPACE_BEGIN()
 
+// FIXME: "(pp)[i] = 0xff" -> SIMD store
+#define CompVCannyHysteresisRowWeak_8mpw_Intrin_SSE2(gg, pp, rr, cc) \
+	vecgg = _mm_loadu_si128(reinterpret_cast<const __m128i*>((gg))); \
+	vecpp = _mm_loadl_epi64(reinterpret_cast<const __m128i*>((pp))); \
+	vecWeak = _mm_and_si128(_mm_cmpeq_epi16(_mm_unpacklo_epi8(vecpp, vecpp), vecZero), _mm_cmpgt_epi16(vecgg, vecTLow)); \
+	vecWeak = _mm_and_si128(vecWeak, vecp); \
+	mask = _mm_movemask_epi8(vecWeak); \
+	if (mask) { \
+		for (size_t i = 0, j = 0; i < 8; ++i, j += 2) { \
+			if (mask & (1 << j) && ((cc) + i) < width) { \
+				(pp)[i] = 0xff; \
+				edges.push_back(CompVMatIndex(rr, (cc) + i)); \
+			} \
+		} \
+	}
+
 // TODO(dmi): no ASM implementation
 // "g" and "tLow" are unsigned but we're using "epi16" instead of "epu16" because "g" is always < 0xFFFF (from u8 convolution operation)
 // 8mpw -> minpack 8 for words (int16)
 void CompVCannyHysteresisRow_8mpw_Intrin_SSE2(size_t row, size_t colStart, size_t width, size_t height, size_t stride, uint16_t tLow, uint16_t tHigh, const uint16_t* grad, const uint16_t* g0, uint8_t* e, uint8_t* e0)
 {
 	COMPV_DEBUG_INFO_CHECK_SSE2();
+#if 1
+	__m128i vecGrad, vecStrong, vecWeak, vecgg, vecpp, vecp;
+	const __m128i vecTLow = _mm_set1_epi16(tLow);
+	const __m128i vecTHigh = _mm_set1_epi16(tHigh);
+	const __m128i vecZero = _mm_setzero_si128();
+	int mask;
+	uint8_t* p;
+	const uint16_t *g, *gb, *gt;
+	size_t c, r, s;
+	uint8_t *pb, *pt;
+	CompVMatIndex edge;
+	bool lookingForStringEdges;
+	// std::vector is faster than std::list, std::dequeue and std::stack (perf. done using Intel VTune on core i7)
+	// also, check https://baptiste-wicht.com/posts/2012/11/cpp-benchmark-vector-vs-list.html
+	std::vector<CompVMatIndex> edges;
+	while (colStart < width - 7 || !edges.empty()) { // width is already >=8 (checked by the caller)
+		if ((lookingForStringEdges = edges.empty())) { // looking for string edges if there is no pending edge in the vector
+			c = colStart;
+			r = row;
+			p = &e[colStart];
+			g = &grad[colStart];
+			colStart += 8; // move to next samples
+		}
+		else {
+			edge = edges.back(); // use saved edges
+			edges.pop_back();
+			c = edge.col;
+			r = edge.row;
+			s = (r * stride) + c;
+			p = e0 + s;
+			g = g0 + s;
+		}
+		if (r && c && r < height && c < width) {
+			vecp = _mm_loadl_epi64(reinterpret_cast<const __m128i*>(p)); // high 64bits set to zero
+			if (lookingForStringEdges) {
+				vecGrad = _mm_loadu_si128(reinterpret_cast<const __m128i*>(g));
+				vecStrong = _mm_and_si128(_mm_cmpeq_epi16(_mm_unpacklo_epi8(vecp, vecp), vecZero), _mm_cmpgt_epi16(vecGrad, vecTHigh));
+				if (_mm_movemask_epi8(vecStrong)) {
+					// write strong edges and update vecp
+					vecp = _mm_or_si128(vecp, _mm_packs_epi16(vecStrong, vecStrong));
+					_mm_storel_epi64(reinterpret_cast<__m128i*>(p), vecp);
+				}
+				else {
+					continue;
+				}
+			}			
+			pb = p + stride;
+			pt = p - stride;
+			gb = g + stride;
+			gt = g - stride;
+			vecp = _mm_unpacklo_epi8(vecp, vecp); // 64bits -> 128bits
+			CompVCannyHysteresisRowWeak_8mpw_Intrin_SSE2(&g[-1], &p[-1], r, c - 1); // left
+			CompVCannyHysteresisRowWeak_8mpw_Intrin_SSE2(&g[1], &p[1], r, c + 1); // right
+			CompVCannyHysteresisRowWeak_8mpw_Intrin_SSE2(&gt[-1], &pt[-1], r - 1, c - 1); // top-left
+			CompVCannyHysteresisRowWeak_8mpw_Intrin_SSE2(&gt[0], &pt[0], r - 1, c); // top-center
+			CompVCannyHysteresisRowWeak_8mpw_Intrin_SSE2(&gt[1], &pt[1], r - 1, c + 1); // top-right
+			CompVCannyHysteresisRowWeak_8mpw_Intrin_SSE2(&gb[-1], &pb[-1], r + 1, c - 1); // bottom-left
+			CompVCannyHysteresisRowWeak_8mpw_Intrin_SSE2(&gb[0], &pb[0], r + 1, c); // bottom-center
+			CompVCannyHysteresisRowWeak_8mpw_Intrin_SSE2(&gb[1], &pb[1], r + 1, c + 1); // bottom-right
+		}
+	}
+#else
 	compv_uscalar_t col, mi;
 	__m128i vecG, vecP, vecGrad, vecE;
 	const __m128i vecTLow = _mm_set1_epi16(tLow);
@@ -116,6 +194,7 @@ void CompVCannyHysteresisRow_8mpw_Intrin_SSE2(size_t row, size_t colStart, size_
 			while (m0);
 		}
 	}
+#endif
 }
 
 void CompVCannyNMSApply_Intrin_SSE2(COMPV_ALIGNED(SSE) uint16_t* grad, COMPV_ALIGNED(SSE) uint8_t* nms, compv_uscalar_t width, compv_uscalar_t height, COMPV_ALIGNED(SSE) compv_uscalar_t stride)
