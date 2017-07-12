17b38a423abf733d28c61e93f0b653f5a3f9d516
Doubango Telecom <github@doubango.org>
2017-07-12 21:15:27 +0200

    -

--- base/image/intrin/arm/compv_image_conv_grayscale_intrin_neon.cxx
+++ base/image/intrin/arm/compv_image_conv_grayscale_intrin_neon.cxx
@@ -39,34 +39,30 @@ COMPV_NAMESPACE_BEGIN()
 		yuv422Ptr += padYUV; \
 	}
 #else
-#define yuv422family_to_y_NEON(comp) \
-    COMPV_DEBUG_INFO_CHECK_NEON(); \
-    compv_uscalar_t i, j, maxI = ((width + 15) & -16), padY = (stride - maxI), padYUV = padY << 1; \
-    uint8x8x2_t vec0, vec1, vec2, vec3; \
-    for (j = 0; j < height; ++j) { \
-        for (i = 0; i < width; i += 16) { \
-            vec0 = { { vld1_u8(&yuv422Ptr[0]), vld1_u8(&yuv422Ptr[8]) } }; \
-            vec1 = { { vld1_u8(&yuv422Ptr[16]), vld1_u8(&yuv422Ptr[24]) } }; \
-            vec2 = vuzp_u8(vec0.val[0], vec0.val[1]); /* vec2.val[0] -> first comp(Y), vec2.val[1] -> second comp(U/V) */ \
-            vec3 = vuzp_u8(vec1.val[0], vec1.val[1]); /* vec3.val[0] -> first comp(Y), vec3.val[1] -> second comp(U/V) */ \
-            vst1q_u8(outYPtr, vcombine_u8(vec2.val[comp], vec3.val[comp])); \
-            outYPtr += 16; \
-            yuv422Ptr += 32; \
-        } \
-        outYPtr += padY; \
-        yuv422Ptr += padYUV; \
-    }
+#define yuv422family_to_y_NEON(Ycomp) { \
+	COMPV_DEBUG_INFO_CHECK_NEON(); \
+	COMPV_DEBUG_INFO_CODE_NOT_OPTIMIZED("ASM32 code #5 times faster (Apple LLVM)"); \
+	compv_uscalar_t i, j, k; \
+	const compv_uscalar_t strideYuv = stride << 1; \
+	uint8x16x2_t vec0; \
+	for (j = 0; j < height; ++j) { \
+		for (i = 0, k = 0; i < width; i += 16, k += 32) { \
+			vec0 = vld2q_u8(&yuv422Ptr[k]); \
+			vst1q_u8(&outYPtr[i], vec0.val[Ycomp]); \
+		} \
+		outYPtr += stride; \
+		yuv422Ptr += strideYuv; \
+	} \
+}
 #endif
 
 void CompVImageConvYuyv422_to_y_Intrin_NEON(COMPV_ALIGNED(NEON) const uint8_t* yuv422Ptr, COMPV_ALIGNED(NEON) uint8_t* outYPtr, compv_uscalar_t width, compv_uscalar_t height, COMPV_ALIGNED(NEON) compv_uscalar_t stride)
 {
-    COMPV_DEBUG_INFO_CODE_NOT_OPTIMIZED("ASM32 code #5 times faster (Apple LLVM)");
-    yuv422family_to_y_NEON(0);
+	yuv422family_to_y_NEON(0);
 }
 
 void CompVImageConvUyvy422_to_y_Intrin_NEON(COMPV_ALIGNED(NEON) const uint8_t* yuv422Ptr, COMPV_ALIGNED(NEON) uint8_t* outYPtr, compv_uscalar_t width, compv_uscalar_t height, COMPV_ALIGNED(NEON) compv_uscalar_t stride)
 {
-    COMPV_DEBUG_INFO_CODE_NOT_OPTIMIZED("ASM32 code #5 times faster (Apple LLVM)");
 	yuv422family_to_y_NEON(1);
 }
 
