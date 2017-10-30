a32bd941aeef42a8cb63c08e263ae26a8fe8a3e5
Doubango Telecom <github@doubango.org>
2017-10-30 09:14:52 +0100

    -

--- core/calib/compv_core_calib_utils.cxx
+++ core/calib/compv_core_calib_utils.cxx
@@ -26,11 +26,11 @@ COMPV_NAMESPACE_BEGIN()
 template<class T>
 class CompVCalibUtilsGeneric {
 public:
-	static COMPV_ERROR_CODE initUndistMap(const CompVSizeSz& imageSize, const CompVMatPtr& K, const CompVMatPtr& d, CompVMatPtrPtr map)
+	static COMPV_ERROR_CODE initUndistMap(const CompVMatPtr& K, const CompVMatPtr& d, CompVMatPtrPtr map, const CompVRectFloat32& roi)
 	{
-		// Internal function, no need to check input parameters. Up to the caller.
+		// Internal function, no need to check *some* input parameters. Up to the caller.
 
-		COMPV_CHECK_EXP_RETURN(!imageSize.width || !imageSize.height || !map || !K || !d || K->subType() != d->subType(), COMPV_ERROR_CODE_E_INVALID_PARAMETER);
+		COMPV_CHECK_EXP_RETURN(!K || !d || K->subType() != d->subType(), COMPV_ERROR_CODE_E_INVALID_PARAMETER);
 		COMPV_CHECK_EXP_RETURN(K->rows() != 3 || K->cols() != 3, COMPV_ERROR_CODE_E_INVALID_PARAMETER, "K must be (3x3) matrix");
 		COMPV_CHECK_EXP_RETURN(d->rows() < 2 || d->cols() != 1, COMPV_ERROR_CODE_E_INVALID_PARAMETER, "d must be (1x4+) vector");
 
@@ -47,8 +47,12 @@ public:
 		const T p1 = d->rows() > 2 ? *d->ptr<const T>(2, 0) : static_cast<T>(0);
 		const T p2 = d->rows() > 3 ? *d->ptr<const T>(3, 0) : static_cast<T>(0);
 
-		const size_t img_width = imageSize.width;
-		const size_t img_height = imageSize.height;
+		const size_t img_start_x = COMPV_MATH_ROUNDFU_2_NEAREST_INT(roi.left, size_t);
+		const size_t img_start_y = COMPV_MATH_ROUNDFU_2_NEAREST_INT(roi.top, size_t);
+		const size_t img_end_x = COMPV_MATH_ROUNDFU_2_NEAREST_INT(roi.right, size_t);
+		const size_t img_end_y = COMPV_MATH_ROUNDFU_2_NEAREST_INT(roi.bottom, size_t);
+		const size_t img_width = (img_end_x - img_start_x) + 1;
+		const size_t img_height = (img_end_y - img_start_y) + 1;
 		const size_t coords_num = (img_width * img_height);
 
 		/* Compute K-inv */
@@ -70,9 +74,9 @@ public:
 		// be called once
 		
 		T x, y, x2, y2, r2, r4, a1, a2, a3, rdist;
-		for (size_t j = 0, k = 0; j < img_height; ++j) {
-			for (size_t i = 0; i < img_width; ++i, ++k) {
-				/* [2] 5.1 step 1: map = mul(Kinv, indices) */
+		for (size_t j = img_start_y, k = 0; j <= img_end_y; ++j) {
+			for (size_t i = img_start_x; i <= img_end_x; ++i, ++k) {
+				/* [2] 5.1 step 1: map = mul(Kinv, p) */
 				x = static_cast<T>(i);
 				y = static_cast<T>(j);
 				x = (kinv11 * x) + (kinv21 * y) + kinv31;
@@ -95,21 +99,112 @@ public:
 				a1 = 2 * (x * y);
 				a2 = r2 + (2 * x2);
 				a3 = r2 + (2 * y2);
-				// add radial distortion
+				// radial distortion
 				rdist = 1 + k1 * r2 + k2 * r4;
 				x *= rdist;
 				y *= rdist;
-				// add tangential distortion
+				// tangential distortion
 				x += p1 * a1 + p2 * a2;
 				y += p1 * a3 + p2 * a1;
 
 				// https://youtu.be/Ou9Uj75DJX0?t=25m34s (3)
-				/* [2] 5.1 step 3: normalized = mul(K, normalized) */
+				/* [2] 5.1 step 3: map = mul(K, p) */
 				mapX[k] = (x * fx) + (skew * y) + cx;
 				mapY[k] = (y * fy) + cy;
 				mapZ[k] = static_cast<T>(1);
 			}
 		}
+		return COMPV_ERROR_CODE_S_OK;
+	}
+
+	// inPoints.z is expected to be equal to 1 and there is no R and T matrices (equal to zero)
+	static COMPV_ERROR_CODE dist2DPoints(const CompVMatPtr& inPoints, const CompVMatPtr& K, const CompVMatPtr& d, CompVMatPtrPtr outPoints)
+	{
+		COMPV_CHECK_EXP_RETURN(!inPoints || inPoints->isEmpty() || inPoints->rows() < 2 || !K || !d || !outPoints, COMPV_ERROR_CODE_E_INVALID_PARAMETER);
+		COMPV_CHECK_EXP_RETURN(K->rows() != 3 || K->cols() != 3, COMPV_ERROR_CODE_E_INVALID_PARAMETER, "K must be (3x3) matrix");
+		COMPV_CHECK_EXP_RETURN(d->rows() < 2 || d->cols() != 1, COMPV_ERROR_CODE_E_INVALID_PARAMETER, "d must be (1x4+) vector");
+		COMPV_CHECK_EXP_RETURN(inPoints->subType() != K->subType() || inPoints->subType() != d->subType(), COMPV_ERROR_CODE_E_INVALID_PARAMETER, "Subtype mismatch");
+
+		// Called several times in Adas project
+		COMPV_DEBUG_INFO_CODE_NOT_OPTIMIZED("No SIMD or GPU implementation found");
+		COMPV_DEBUG_INFO_CODE_NOT_OPTIMIZED("No MT implementation found");
+
+		CompVPointFloat32Vector::const_iterator it_intersections;
+		size_t index;
+		const size_t numPoints = inPoints->cols();
+
+		// If 'inPoints' == 'outPoints' this is very good for us beacuse we
+		// can avoid creating new CompVMat object
+		if (inPoints != *outPoints) {
+			COMPV_CHECK_CODE_RETURN(CompVMat::newObjAligned<T>(outPoints, 2, numPoints));
+		}
+
+		T* outPointsX = (*outPoints)->ptr<T>(0);
+		T* outPointsY = (*outPoints)->ptr<T>(1);
+
+		const T* inPointsX = inPoints->ptr<T>(0);
+		const T* inPointsY = inPoints->ptr<T>(1);
+
+		// Camera matrix
+		const T fx = *K->ptr<const T>(0, 0);
+		const T fy = *K->ptr<const T>(1, 1);
+		const T cx = *K->ptr<const T>(0, 2);
+		const T cy = *K->ptr<const T>(1, 2);
+		const T skew = *K->ptr<const T>(0, 1);
+
+		// Radial and tangential distorsion coefficients
+		const T k1 = *d->ptr<const T>(0, 0);
+		const T k2 = *d->ptr<const T>(1, 0);
+		const T p1 = d->rows() > 2 ? *d->ptr<const T>(2, 0) : static_cast<T>(0);
+		const T p2 = d->rows() > 3 ? *d->ptr<const T>(3, 0) : static_cast<T>(0);
+
+		/* Compute K-inv */
+		T detKinv = (fx * fy);
+		COMPV_CHECK_EXP_RETURN(!detKinv, COMPV_ERROR_CODE_E_INVALID_CALL, "Camera matrix is singular");
+		detKinv = (1 / detKinv);
+		const T kinv11 = (fy * detKinv);
+		const T kinv21 = -(skew * detKinv);
+		const T kinv31 = ((skew * cy) - (fy * cx)) * detKinv;
+		const T kinv22 = (fx * detKinv);
+		const T kinv32 = -(cy * fx * detKinv);
+
+		T x, y;
+		T x2, y2, r2, r4, a1, a2, a3, rdist;
+		for (index = 0; index < numPoints; ++index) {
+			// p = mul(Kinv, p)
+			x = inPointsX[index];
+			y = inPointsY[index];
+			x = (kinv11 * x) + (kinv21 * y) + kinv31;
+			y = (kinv22 * y) + kinv32;
+
+#if 0 // z, always equal to 1 last row for kinv is "0, 0, 1"
+			COMPV_DEBUG_INFO_CODE_TODO("Kinv last line should be 0 0 1 which means z is always equal 1, remove next code");
+			z = z ? (static_cast<T>(1) / z) : static_cast<T>(1);
+			x *= z;
+			y *= z;
+#endif
+
+			// Radial and tangential distorsions
+			x2 = (x * x);
+			y2 = (y * y);
+			r2 = x2 + y2;
+			r4 = r2 * r2;
+			a1 = 2 * (x * y);
+			a2 = r2 + (2 * x2);
+			a3 = r2 + (2 * y2);
+			// radial distortion
+			rdist = 1 / (1 + k1 * r2 + k2 * r4);
+			x *= rdist; // inverse: div instead of mul
+			y *= rdist; // inverse: div instead of mul
+			// tangential distortion
+			x -= p1 * a1 + p2 * a2; // inverse: sub instead of add
+			y -= p1 * a3 + p2 * a1; // inverse: sub instead of add
+
+			// outPoints = mul(K, outPoints)
+			outPointsX[index] = (x * fx) + (skew * y) + cx;
+			outPointsY[index] = (y * fy) + cy;
+		}
+
 		return COMPV_ERROR_CODE_S_OK;
 	}
 };
@@ -308,14 +403,29 @@ COMPV_ERROR_CODE CompVCalibUtils::proj2DError(const CompVCalibCameraPlanVector& 
 	return COMPV_ERROR_CODE_S_OK;
 }
 
-COMPV_ERROR_CODE CompVCalibUtils::initUndistMap(const CompVSizeSz& imageSize, const CompVMatPtr& K, const CompVMatPtr& d, CompVMatPtrPtr map)
+COMPV_ERROR_CODE CompVCalibUtils::initUndistMap(const CompVSizeSz& imageSize, const CompVMatPtr& K, const CompVMatPtr& d, CompVMatPtrPtr map, const CompVRectFloat32* imageROI COMPV_DEFAULT(nullptr))
 {
 	// Other parameters will be checked in the generic function
 	COMPV_CHECK_EXP_RETURN(!K || !d || K->subType() != d->subType(), COMPV_ERROR_CODE_E_INVALID_PARAMETER);
+
+	CompVRectFloat32 roi;
+	if (imageROI) {
+		COMPV_CHECK_EXP_RETURN(imageROI->left < 0.f || imageROI->right < 0.f || imageROI->top < 0.f || imageROI->bottom < 0.f
+			|| imageROI->left > imageROI->right || imageROI->top > imageROI->bottom
+			|| imageROI->right >= imageSize.width || imageROI->bottom >= imageSize.height,
+			COMPV_ERROR_CODE_E_INVALID_PARAMETER, "ROI is invalid");
+		roi = *imageROI;
+	}
+	else {
+		roi.left = 0;
+		roi.right = static_cast<compv_float32_t>(imageSize.width - 1);
+		roi.top = 0;
+		roi.bottom = static_cast<compv_float32_t>(imageSize.height - 1);
+	}
 	
 	switch (K->subType()) {
-	case COMPV_SUBTYPE_RAW_FLOAT32: COMPV_CHECK_CODE_RETURN((CompVCalibUtilsGeneric<compv_float32_t>::initUndistMap(imageSize, K, d, map))); break;
-	case COMPV_SUBTYPE_RAW_FLOAT64: COMPV_CHECK_CODE_RETURN((CompVCalibUtilsGeneric<compv_float64_t>::initUndistMap(imageSize, K, d, map))); break;
+	case COMPV_SUBTYPE_RAW_FLOAT32: COMPV_CHECK_CODE_RETURN((CompVCalibUtilsGeneric<compv_float32_t>::initUndistMap(K, d, map, roi))); break;
+	case COMPV_SUBTYPE_RAW_FLOAT64: COMPV_CHECK_CODE_RETURN((CompVCalibUtilsGeneric<compv_float64_t>::initUndistMap(K, d, map, roi))); break;
 	default: COMPV_CHECK_CODE_RETURN(COMPV_ERROR_CODE_E_NOT_IMPLEMENTED, "Camera intrinsics and distorsion coefficients must be floats (32f) or doubles (64f)");  break;
 	}
 	return COMPV_ERROR_CODE_S_OK;
@@ -338,14 +448,36 @@ COMPV_ERROR_CODE CompVCalibUtils::undist2DImage(const CompVMatPtr& imageIn, cons
 }
 
 // map -> must be generated using CompVCalibUtils::initUndistMap
-COMPV_ERROR_CODE CompVCalibUtils::undist2DImage(const CompVMatPtr& imageIn, const CompVMatPtr& map, CompVMatPtrPtr imageOut, COMPV_INTERPOLATION_TYPE interpType COMPV_DEFAULT(COMPV_INTERPOLATION_TYPE_BILINEAR), const CompVRectFloat32* mapROI COMPV_DEFAULT(nullptr))
+COMPV_ERROR_CODE CompVCalibUtils::undist2DImage(const CompVMatPtr& imageIn, const CompVMatPtr& map, CompVMatPtrPtr imageOut, COMPV_INTERPOLATION_TYPE interpType COMPV_DEFAULT(COMPV_INTERPOLATION_TYPE_BILINEAR), const CompVRectFloat32* imageInROI COMPV_DEFAULT(nullptr))
 {
 	// Other parameters will be checked in the generic function
 	COMPV_CHECK_EXP_RETURN(!imageIn || imageIn->isEmpty() || !map, COMPV_ERROR_CODE_E_INVALID_PARAMETER);
 
-	// Do not check that map and imageIn have the same size, this will be done in CompVImageRemap::process
-	COMPV_CHECK_CODE_RETURN(CompVImageRemap::process(imageIn, imageOut, map, interpType, mapROI));
+	// If 'imageInROI' is non-null then, the map should have been created using the same roi
 
+	// Do not check that map and imageIn have the same size, this will be done in CompVImageRemap::process
+	COMPV_CHECK_CODE_RETURN(CompVImageRemap::process(imageIn, imageOut, map, interpType, imageInROI));
+
+	return COMPV_ERROR_CODE_S_OK;
+}
+
+// inPoints.z is expected to be equal to 1 and there is no R and T matrices (equal to zero)
+COMPV_ERROR_CODE CompVCalibUtils::dist2DPoints(const CompVMatPtr& inPoints, const CompVMatPtr& K, const CompVMatPtr& d, CompVMatPtrPtr outPoints)
+{
+	// Other parameters will be checked in the generic function
+	COMPV_CHECK_EXP_RETURN(!K || !d || K->subType() != d->subType(), COMPV_ERROR_CODE_E_INVALID_PARAMETER);
+
+	switch (K->subType()) {
+	case COMPV_SUBTYPE_RAW_FLOAT32: COMPV_CHECK_CODE_RETURN((CompVCalibUtilsGeneric<compv_float32_t>::dist2DPoints(inPoints, K, d, outPoints))); break;
+	case COMPV_SUBTYPE_RAW_FLOAT64: COMPV_CHECK_CODE_RETURN((CompVCalibUtilsGeneric<compv_float64_t>::dist2DPoints(inPoints, K, d, outPoints))); break;
+	default: COMPV_CHECK_CODE_RETURN(COMPV_ERROR_CODE_E_NOT_IMPLEMENTED, "Camera intrinsics and distorsion coefficients must be floats (32f) or doubles (64f)");  break;
+	}
+	return COMPV_ERROR_CODE_S_OK;
+}
+
+COMPV_ERROR_CODE CompVCalibUtils::dist2DPoints(CompVMatPtr& inOutPoints, const CompVMatPtr& K, const CompVMatPtr& d)
+{
+	COMPV_CHECK_CODE_RETURN(CompVCalibUtils::dist2DPoints(inOutPoints, K, d, &inOutPoints));
 	return COMPV_ERROR_CODE_S_OK;
 }
 
