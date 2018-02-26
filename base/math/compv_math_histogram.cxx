82c977d0e32f9c7c12d990557ec7e9f31358c054
Doubango Telecom <github@doubango.org>
2018-02-26 23:40:53 +0100

    -

--- base/math/compv_math_histogram.cxx
+++ base/math/compv_math_histogram.cxx
@@ -140,7 +140,7 @@ COMPV_ERROR_CODE CompVMathHistogram::buildProjectionX(const CompVMatPtr& dataIn,
 #elif COMPV_ARCH_ARM
 	if (width >= 16 && CompVCpu::isEnabled(kCpuFlagARM_NEON) && dataIn->isAlignedNEON() && ptr32sProjection_->isAlignedNEON()) {
 		COMPV_EXEC_IFDEF_INTRIN_ARM((CompVMathHistogramBuildProjectionX_8u32s = CompVMathHistogramBuildProjectionX_8u32s_Intrin_NEON));
-		//COMPV_EXEC_IFDEF_ASM_ARM32((CompVMathHistogramBuildProjectionX_8u32s = CompVMathHistogramBuildProjectionX_8u32s_Asm_X64_SSE2));
+		COMPV_EXEC_IFDEF_ASM_ARM32((CompVMathHistogramBuildProjectionX_8u32s = CompVMathHistogramBuildProjectionX_8u32s_Asm_NEON32));
 	}
 #endif
 
