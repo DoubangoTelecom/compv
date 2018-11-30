44e470630a59c0d35cd34fd7c7decff0d2597ffe
Doubango Telecom <github@doubango.org>
2018-11-30 21:54:05 +0100

    -

--- plugin_opencl/include/compv/cl/compv_cl_config.h
+++ plugin_opencl/include/compv/cl/compv_cl_config.h
@@ -27,10 +27,10 @@
 #	define COMPV_CL_API
 #endif
 
-#define COMPV_CHECK_CL_CODE_NOP(clerr, ...) do { cl_int __clerr__ = (clerr); if (clerr != CL_SUCCESS) { COMPV_DEBUG_ERROR_EX("OpenCL", "OpenCL operation failed (%d -> %s) -> " ##__VA_ARGS__, __clerr__, COMPV_NAMESPACE::CompVCLUtils::errorString(__clerr__)); } } while(0)
-#define COMPV_CHECK_CL_CODE_BAIL(clerr, ...) do { cl_int __clerr__ = (clerr); if (clerr != CL_SUCCESS) { COMPV_DEBUG_ERROR_EX("OpenCL", "OpenCL operation failed (%d -> %s) -> " ##__VA_ARGS__, __clerr__, COMPV_NAMESPACE::CompVCLUtils::errorString(__clerr__)); goto bail; } } while(0)
-#define COMPV_CHECK_CL_CODE_RETURN(clerr, ...) do { cl_int __clerr__ = (clerr); if (clerr != CL_SUCCESS) { COMPV_DEBUG_ERROR_EX("OpenCL", "OpenCL operation failed (%d -> %s) -> " ##__VA_ARGS__, __clerr__, COMPV_NAMESPACE::CompVCLUtils::errorString(__clerr__)); return __clerr__; } } while(0)
-#define COMPV_CHECK_CL_CODE_ASSERT(clerr, ...) do { cl_int __clerr__ = (clerr); if (clerr != CL_SUCCESS) { COMPV_DEBUG_ERROR_EX("OpenCL", "OpenCL operation failed (%d -> %s) -> " ##__VA_ARGS__, __clerr__, COMPV_NAMESPACE::CompVCLUtils::errorString(__clerr__)); COMPV_ASSERT(false); } } while(0)
+#define COMPV_CHECK_CL_CODE_NOP(clerr, ...) do { cl_int __clerr__ = (clerr); if (__clerr__ != CL_SUCCESS) { COMPV_DEBUG_ERROR_EX("OpenCL", "OpenCL operation failed (%d -> %s) -> " ##__VA_ARGS__, __clerr__, COMPV_NAMESPACE::CompVCLUtils::errorString(__clerr__)); } } while(0)
+#define COMPV_CHECK_CL_CODE_BAIL(clerr, ...) do { cl_int __clerr__ = (clerr); if (__clerr__ != CL_SUCCESS) { COMPV_DEBUG_ERROR_EX("OpenCL", "OpenCL operation failed (%d -> %s) -> " ##__VA_ARGS__, __clerr__, COMPV_NAMESPACE::CompVCLUtils::errorString(__clerr__)); goto bail; } } while(0)
+#define COMPV_CHECK_CL_CODE_RETURN(clerr, ...) do { cl_int __clerr__ = (clerr); if (__clerr__ != CL_SUCCESS) { COMPV_DEBUG_ERROR_EX("OpenCL", "OpenCL operation failed (%d -> %s) -> " ##__VA_ARGS__, __clerr__, COMPV_NAMESPACE::CompVCLUtils::errorString(__clerr__)); return __clerr__; } } while(0)
+#define COMPV_CHECK_CL_CODE_ASSERT(clerr, ...) do { cl_int __clerr__ = (clerr); if (__clerr__ != CL_SUCCESS) { COMPV_DEBUG_ERROR_EX("OpenCL", "OpenCL operation failed (%d -> %s) -> " ##__VA_ARGS__, __clerr__, COMPV_NAMESPACE::CompVCLUtils::errorString(__clerr__)); COMPV_ASSERT(false); } } while(0)
 #define COMPV_CHECK_CL_EXP_NOP(exp, clerr, ...) do { if ((exp)) COMPV_CHECK_CL_CODE_NOP(hr,  ##__VA_ARGS__); } while(0)
 #define COMPV_CHECK_CL_EXP_RETURN(exp, clerr, ...) do { if ((exp)) COMPV_CHECK_CL_CODE_RETURN(hr,  ##__VA_ARGS__); } while(0)
 #define COMPV_CHECK_CL_EXP_BAIL(exp, clerr, ...) do { if ((exp)) COMPV_CHECK_CL_CODE_BAIL(hr,  ##__VA_ARGS__); } while(0)
